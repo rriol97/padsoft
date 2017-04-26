c0868906ed1854d307bf4897e54f57b868db4063
rriol97 <ricardo.riol@estudiante.uam.es>
2017-04-27 00:26:00 +0200

    Add files via upload
    Lista de inicio alumno medio hecha

--- AulaVirtual/src/aplicacion/GUI/componentes/PanelSolAsig.java
+++ AulaVirtual/src/aplicacion/GUI/componentes/PanelSolAsig.java
@@ -5,20 +5,36 @@ import java.awt.BorderLayout;
 import java.util.List;
 
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 
-public class PanelSolAsig extends JPanel {
+public class PanelSolAsig extends JPanel implements ListSelectionListener {
 	private static final long serialVersionUID = 1L;
+    private JList listOne;
+    private JScrollPane scrollingListOne;
+    private String[]dataList;
 	
-	public PanelSolAsig(List <String> nombres) {
-		this.setLayout(new BorderLayout());
-		
-		JList lista_asig = new JList ();
-		
-		for (String n: nombres) {
-			
-		}
+	public PanelSolAsig(String[]dataList) {        
+        listOne = new JList(dataList);
+        scrollingListOne = new JScrollPane(listOne);
+        this.dataList = dataList;
+                
+        this.add(scrollingListOne);
+                
+        // listener para la lista
+        listOne.addListSelectionListener(this); 
+        listOne.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
 	}
+
+    public void valueChanged(ListSelectionEvent e) {               
+        int selection = this.listOne.getSelectedIndex(); 
+        JOptionPane.showMessageDialog(null, dataList[selection]);
+    } 
 	
 }
--- AulaVirtual/src/aplicacion/GUI/general/FrameTester.java
+++ AulaVirtual/src/aplicacion/GUI/general/FrameTester.java
@@ -1,17 +1,28 @@
 package aplicacion.GUI.general;
 
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import aplicacion.GUI.componentes.PanelSolAsig;
 
 public class FrameTester {
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		List<String> l = new ArrayList<String>();
-		l.add("aaaaaa");
-		l.add("bbbbbbbbbbbbbbbbbbbbbbbb");
-		f.cambiarPanel(new PanelSolAsig(l));
-	
+		String[] dataList = {"Andalucia", "Aragon", "Asturias", "Baleares", 
+                 "Canarias", "Cantabria", "Castilla la Mancha", 
+                 "Castilla y Leon", "Catalunya", "Extremadura", 
+                 "Galicia", "Madrid", "Murcia", "Navarra", 
+                 "Pais Vasco", "La Rioja", "Pais Valenciano", 
+                 "Ceuta", "Melilla"};
+		//f.cambiarPanel(new PanelSolAsig());
+		PanelSolAsig a = new PanelSolAsig(dataList);
+		f.add(a, BorderLayout.WEST);
+		//f.setSize(200, 200);
+		f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
