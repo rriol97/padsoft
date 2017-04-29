e2799b4db2ad4fc8238c7d09a7388c203f46bae0
rriol97 <ricardo.riol@estudiante.uam.es>
2017-04-29 18:56:15 +0200

    Add files via upload

--- AulaVirtual/src/aplicacion/GUI/general/PanelSolAsig.java
+++ AulaVirtual/src/aplicacion/GUI/general/PanelSolAsig.java
@@ -1,7 +1,9 @@
-package aplicacion.GUI.general;
+package aplicacion.GUI.componentes;
 
 import java.awt.Button;
 import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,35 +11,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-import aplicacion.clases.Aplicacion;
-import aplicacion.clases.Asignatura;
+import aplicacion.GUI.general.Frame;
 
 public class PanelSolAsig extends JPanel {
-	private static final long serialVersionUID = 1L;
-	
-	private JComboBox<String> listaAsig;
+    private JComboBox listaAsig;
     private JScrollPane scrollingAsig;
  
     
-    public PanelSolAsig (Aplicacion ap){
+    public PanelSolAsig (String [] asig){
 		SpringLayout layout = new SpringLayout();
 		this.setLayout(layout);
-		
-		String[] lista_asig = new String[ap.getAsignaturas().size()];
-		int i = 0;
-		for (Asignatura asig: Aplicacion.getInstance().getAsignaturas()) {
-			lista_asig[i] = asig.getNombre();
-			i++;
-		}
-		
-		this.listaAsig = new JComboBox<String>(lista_asig);
+  
+        listaAsig = new JComboBox(asig);
         JLabel etiq = new JLabel("Asignaturas");
-        this.scrollingAsig = new JScrollPane(listaAsig);
-        this.scrollingAsig.setPreferredSize(new Dimension(200,30));
+        etiq.setFont(new Font("Arial",12,18));
+        scrollingAsig = new JScrollPane(listaAsig);
+        scrollingAsig.setPreferredSize(new Dimension(200,30));
         JTextField texto = new JTextField(30);
         JLabel coment = new JLabel ("Comentario");
+        coment.setFont(new Font("Arial",12,18));
         texto.setPreferredSize(new Dimension((int)(Frame.WIDTH/23),(int)(Frame.HEIGHT/23)));
-        Button botonAcp = new Button ("Aceptar");
+        Button botonAcp = new Button ("Enviar");
         botonAcp.setPreferredSize(new Dimension((int)Frame.WIDTH/20,(int)Frame.HEIGHT/35));
         Button botonCnr = new Button ("Cancelar");
         botonCnr.setPreferredSize(new Dimension((int)Frame.WIDTH/18,(int)Frame.HEIGHT/35));
@@ -49,16 +43,16 @@ public class PanelSolAsig extends JPanel {
         this.add(botonAcp);
         this.add(botonCnr);
         
-        layout.putConstraint(SpringLayout.WEST,etiq ,30,SpringLayout.WEST ,this);
-        layout.putConstraint(SpringLayout.NORTH,etiq ,5,SpringLayout.SOUTH ,this);
+        layout.putConstraint(SpringLayout.NORTH,etiq,200 ,SpringLayout.NORTH, this);
+        layout.putConstraint(SpringLayout.WEST,etiq ,200,SpringLayout.WEST ,this);
         layout.putConstraint(SpringLayout.WEST,scrollingAsig,20, SpringLayout.EAST,etiq);
-        layout.putConstraint(SpringLayout.NORTH,scrollingAsig,5, SpringLayout.SOUTH,this);
+        layout.putConstraint(SpringLayout.NORTH,scrollingAsig,200, SpringLayout.NORTH,this);
         layout.putConstraint(SpringLayout.NORTH,coment,25, SpringLayout.SOUTH,etiq);
-        layout.putConstraint(SpringLayout.WEST,coment, 30,SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.WEST,coment,200,SpringLayout.WEST, this);
         layout.putConstraint(SpringLayout.WEST,texto,10, SpringLayout.EAST,coment);
         layout.putConstraint(SpringLayout.NORTH,texto,15, SpringLayout.SOUTH,scrollingAsig);
         layout.putConstraint(SpringLayout.NORTH,botonCnr,60, SpringLayout.SOUTH,texto);
-        layout.putConstraint(SpringLayout.WEST,botonCnr,350, SpringLayout.WEST,this);
+        layout.putConstraint(SpringLayout.WEST,botonCnr,500, SpringLayout.WEST,this);
         layout.putConstraint(SpringLayout.WEST,botonAcp,3, SpringLayout.EAST,botonCnr);
         layout.putConstraint(SpringLayout.NORTH,botonAcp,60, SpringLayout.SOUTH,texto);
         
