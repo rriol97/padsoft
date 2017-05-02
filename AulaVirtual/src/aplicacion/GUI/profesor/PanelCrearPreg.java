76c62ed8def2f589ce943d5919faaa43567768f0
Ricardo <Ricardo@Ricardo>
2017-05-02 18:02:58 +0200

    Empezado funcionalidad crear examenes

---
+++ AulaVirtual/src/aplicacion/GUI/profesor/PanelCrearPreg.java
@@ -0,0 +1,43 @@
+package aplicacion.GUI.profesor;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+
+import aplicacion.clases.elemento.test.Test;
+
+public class PanelCrearPreg extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private JButton anadir;
+	private JComboBox<String> tipoPregunta;
+	private JLabel enun;
+	private JTextArea enunciado;
+	
+	
+	public PanelCrearPreg (Test test){
+		this.setLayout(new SpringLayout());
+		
+		//Panel con el botn de añadir asignatura
+		JPanel preg = new JPanel();
+		SpringLayout layoutAna = new SpringLayout();
+		preg.setLayout(layoutAna);
+		this.anadir = new JButton ("Añadir Pregunta");
+		preg.add(this.anadir);
+		layoutAna.putConstraint(SpringLayout.NORTH,this.anadir, 10, SpringLayout.NORTH, preg);
+		preg.setVisible(true);
+		
+		//Panel con el boton del tipo de pregunta
+		JPanel tipoPreg = new JPanel();
+		SpringLayout layoutTipo = new SpringLayout();
+		String[]tipos = {"Respuesta única", "Respuesta múltiple", "Si/No","Respuesta corta"};
+		this.tipoPregunta = new JComboBox<String>(tipos);
+		tipoPreg.add(this.tipoPregunta);
+		layoutTipo.putConstraint(SpringLayout.NORTH,this.anadir, 10, SpringLayout.NORTH, preg);
+		tipoPreg.setVisible(false);
+		
+		//Panel para escribir el anunciado
+	}
+}
--- AulaVirtual/src/aplicacion/GUI/profesor/PanelCrearTest.java
+++ AulaVirtual/src/aplicacion/GUI/profesor/PanelCrearTest.java
@@ -1,19 +1,31 @@
 package aplicacion.GUI.profesor;
 
+import java.time.LocalDate;
+import java.util.Properties;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
+import org.jdatepicker.impl.DateComponentFormatter;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
 import aplicacion.GUI.SpringUtilities;
 import aplicacion.GUI.acciones.ActionVolverAsig;
+import aplicacion.GUI.general.Frame;
 import aplicacion.clases.Asignatura;
 
 public class PanelCrearTest extends PanelComun {
 	private static final long serialVersionUID = 1L;
 	private JComboBox<String> ordenPreguntas;
-	//private JComboBox<String> fechaInicio;
-	private JComboBox<String> visibilidad;
-
+	private JLabel fi;
+	private JLabel ff;
+	private JDatePickerImpl fechaIni;
+	private JDatePickerImpl fechaFin;
+	private JButton anadir;
 	
 	public PanelCrearTest(Asignatura asig){
 		super();
@@ -23,17 +35,46 @@ public class PanelCrearTest extends PanelComun {
 		//this.aceptar.addActionListener(new ActionCrearApuntes(asig,this));
 		
 		JPanel panel_elecciones = new JPanel();
-		panel_elecciones.setLayout(new SpringLayout());
+		SpringLayout layout = new SpringLayout();
+		panel_elecciones.setLayout(layout);
 		String[]orden = {"Ordenadas","No Ordenadas"};
 		this.ordenPreguntas = new JComboBox<String>(orden);
 		panel_elecciones.add(this.ordenPreguntas);
 		
+		fi = new JLabel("Fecha Inicial");
+		ff = new JLabel ("Fecha Fin");
+		panel_elecciones.add(fi);
+		panel_elecciones.add(ff);
+		
+		JPanel preg = new JPanel();
+		SpringLayout layoutAna = new SpringLayout();
+		preg.setLayout(layoutAna);
+		this.anadir = new JButton ("Añadir Pregunta");
+		preg.add(this.anadir);
+		//layoutAna.putConstraint(SpringLayout.NORTH,this.anadir, 10, SpringLayout.NORTH, preg);
+		preg.setVisible(true);
+		
+		UtilDateModel model = new UtilDateModel();
+		model.setDate(2017, 5, 2);
+		model.setSelected(true);
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, new Properties());
+		this.fechaIni = new JDatePickerImpl(datePanel, new DateComponentFormatter());
+		this.fechaFin = new JDatePickerImpl(datePanel,  new DateComponentFormatter());
+		panel_elecciones.add(this.fechaIni);
+		panel_elecciones.add(fechaFin);
+		layout.putConstraint(SpringLayout.NORTH,this.fi,(int) Frame.HEIGHT/10 ,SpringLayout.NORTH, this.ordenPreguntas);
+		layout.putConstraint(SpringLayout.NORTH,this.ff,(int) Frame.HEIGHT/10 ,SpringLayout.NORTH, this.fi);
+		layout.putConstraint(SpringLayout.NORTH,this.fechaIni,(int) Frame.HEIGHT/10 ,SpringLayout.NORTH, this.ordenPreguntas);
+		layout.putConstraint(SpringLayout.NORTH,this.fechaFin,(int) Frame.HEIGHT/10 ,SpringLayout.NORTH, this.fechaIni);
+		layout.putConstraint(SpringLayout.WEST,this.fechaIni,(int) Frame.HEIGHT/8 ,SpringLayout.WEST, this.fi);
+		layout.putConstraint(SpringLayout.WEST,this.fechaFin,(int) Frame.HEIGHT/8,SpringLayout.WEST, this.ff);
 		this.add(aux);
+		this.add(preg);
 		this.add(panel_elecciones);
 		this.add(panel_botones);
 		
 		
-		SpringUtilities.makeCompactGrid(this, 3, 1, 5, 5, 5, 5);
+		SpringUtilities.makeCompactGrid(this, 4, 1, 5, 5, 5, 5);
 		
 	}
 	
@@ -43,5 +84,13 @@ public class PanelCrearTest extends PanelComun {
 	
 	public String getOrden(){
 		return (String) this.ordenPreguntas.getSelectedItem();
+	}
+	
+	public LocalDate getFechaIni(){
+		return (LocalDate) this.fechaIni.getModel().getValue();
+	}
+	
+	public LocalDate getFechaFin(){
+		return (LocalDate) this.fechaFin.getModel().getValue();
 	}
 }
